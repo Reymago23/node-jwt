e7f4aa1787cff2aa96e101540ba8e3fa3dbbbb56
Reymago23 <elderriqui@gmail.com>
2022-09-27 19:26:31 -0600

    add initial jwt logic

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,17 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+
 
 const login = async (req, res) => {
-    res.send('Fake route')
+    const { username, password } = req.body
+    // console.log(username, password);
+    if (!username || !password) {
+        throw new CustomAPIError('invalid username or password', 400)
+    }
+    const id = new Date().getDate()
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
+
+    res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
